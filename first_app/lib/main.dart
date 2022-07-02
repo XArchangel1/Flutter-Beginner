7482015eb5eb042c3f6cff6a55e77b7deed48f92
naksh <nakshatrasemwal1@gmail.com>
2022-07-02 17:58:51 +0530

    made pages folder

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:first_app/homepage.dart';
+import 'package:first_app/pages/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:first_app/loginPage.dart';
 import 'package:google_fonts/google_fonts.dart';
