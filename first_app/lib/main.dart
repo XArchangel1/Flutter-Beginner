4d7c93fe10724feb6e7b3c5f3b4bf1f7e808f2f5
naksh <nakshatrasemwal1@gmail.com>
2022-07-03 17:34:08 +0530

    some changes

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:first_app/pages/homepage.dart';
 import 'package:first_app/utils/routes.dart';
 import 'package:flutter/material.dart';
--- first_app/lib/pages/loginPage.dart
+++ first_app/lib/pages/loginPage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, use_build_context_synchronously
+
 import 'package:first_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 
