4a4bad17b698236ebe08ba05445819ae434da8e3
naksh <nakshatrasemwal1@gmail.com>
2022-07-03 16:53:25 +0530

    ripple effect solved

--- first_app/lib/pages/loginPage.dart
+++ first_app/lib/pages/loginPage.dart
@@ -55,37 +55,36 @@ class _LoginPageState extends State<LoginPage> {
                   SizedBox(
                     height: 20.0,
                   ),
-                  InkWell(
-                    onTap: () async{
-                      setState(() {
-                        changeButton = true;
-                      });
-
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    },
-                    child: AnimatedContainer(
-                        duration: Duration(seconds: 1),
-                        width: changeButton?50:150,
-                        height: 50,
-                        alignment: Alignment.center,
-                        child: changeButton?
-                        Icon(
-                          Icons.done,
-                          color:Colors.white
-                          ):
-                        Text(
-                          "Login",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                            )),
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          //shape:changeButton? BoxShape.circle:BoxShape.rectangle,
-                          borderRadius: BorderRadius.circular(changeButton?50:8),
-                        )),
+                  Material(
+                    color: Colors.deepPurple,
+                    borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 8),
+                    child: InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 50 : 150,
+                          height: 50,
+                          alignment: Alignment.center,
+                          child: changeButton
+                              ? Icon(Icons.done, color: Colors.white)
+                              : Text("Login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  )),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            //shape:changeButton? BoxShape.circle:BoxShape.rectangle,
+                          )),
+                    ),
                   )
 
                   /*ElevatedButton(
